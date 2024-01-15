0ae56c07873dfb756acc14f8e01c2fbb95ca3363
park-seok-hoon <tjrgnsdx1@naver.com>
2024-01-15 20:36:53 +0900

    리필

--- src/homework3/university/Lecture.java
+++ src/homework3/university/Lecture.java
@@ -2,14 +2,46 @@ package homework3.university;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import lombok.Data;
 
 @Data
 public class Lecture {
 	
+	
 	int lecnumber; //강의 번호
-	String name; //강의 이름
+	String lecname; //강의 이름
+	int acceptnumber=0; //현재 수용 강의 인원
+	int maxacceptnumber; //최대 수용 강의 인원
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lecture other = (Lecture) obj;
+		return Objects.equals(lecname, other.lecname) && lecnumber == other.lecnumber;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(lecname, lecnumber);
+	}
+	
+	
+	public Lecture(int lecnumber, String lecname) { //강의 번호,이름을 입력받은 것을 집어넣을 생성자
+		this.lecnumber = lecnumber;
+		this.lecname = lecname;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- src/homework3/university/Professor.java
+++ src/homework3/university/Professor.java
@@ -2,6 +2,7 @@ package homework3.university;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Professor {
 	
@@ -10,17 +11,42 @@ public class Professor {
 //	입학년도 + 과코드 + 2자리
 //	
 //	특별한숫자 + 과코드 + 2자리 
-	private int age;
-	private String name;
+	private int age;	//교수 나이
+	private String name; //교수 이름
 	
+	List<Lecture> lecture=new ArrayList<Lecture>();
 	
 	
 	//(강의1, 강의1 정원현황(0/20)) (전공) 
 	
+	
+
+
+	@Override
+	public int hashCode() {	
+		return Objects.hash(id);
+	}
+
+
 	@Override
 	public String toString() {
-		return "교수 [ 나이 = " + age + "이름 =" + name + ",핸드폰 번호 =" + ",이메일 =";
+		return "Professor [ 교수 번호 = " + id + ", 나이 = " + age + ", 이름 = " + name + " ]";
 	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		return id == other.id;
+	}
+	
+	
 	
 	
 	
