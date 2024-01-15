53dc22760a713ab4b4874b8e799dc45e688e291a
park-seok-hoon <tjrgnsdx1@naver.com>
2024-01-15 20:34:31 +0900

    gitignore 생성

--- src/homework3/university/Professor.java
+++ src/homework3/university/Professor.java
@@ -1,16 +1,25 @@
 package homework3.university;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor {
+	
+	private int id; //교수 고유 번호
+	
+//	입학년도 + 과코드 + 2자리
+//	
+//	특별한숫자 + 과코드 + 2자리 
 	private int age;
 	private String name;
-	private String phonenumber;
-	private String email;
 	
 	
+	
+	//(강의1, 강의1 정원현황(0/20)) (전공) 
 	
 	@Override
 	public String toString() {
-		return "교수 [ 나이 = " + age + "이름 =" + name + ",핸드폰 번호 =" + phonenumber + ",이메일 =" + email + "]";
+		return "교수 [ 나이 = " + age + "이름 =" + name + ",핸드폰 번호 =" + ",이메일 =";
 	}
 	
 	
--- src/homework3/university/Student.java
+++ src/homework3/university/Student.java
@@ -1,13 +1,63 @@
 package homework3.university;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import lombok.Data;
+
 
 @Data
 public class Student {
-	private int age;
-	private String name;
-	private String phonenumber;
-	private String email;
+	private int universenum; //학번
+	private int age; 	//나이
+	private String name; //이름
+	
+	List<Lecture> lecture=new ArrayList<Lecture>(); //강의 리스트
+	
+	
+	int score;
+
+
+	@Override
+	public String toString() {
+		return "학생 [학번 = " + universenum + ", age = " + age + ", name = " + name + ", lecture = " + lecture
+				+ ", score = " + score + "]";
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return universenum == other.universenum;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(universenum);  //학번 
+	}
+
+
+	public Student(int universenum, int age, String name) {	//학번,나이,이름 입력 받으면 집어넣을 생성자
+		super();
+		this.universenum = universenum;
+		this.age = age;
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
