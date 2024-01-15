c841423a139d30995e24c1a4736e093549326b80
park-seok-hoon <tjrgnsdx1@naver.com>
2024-01-15 20:35:59 +0900

    Create Lecture.java

---
+++ src/homework3/university/Lecture.java
@@ -0,0 +1,17 @@
+package homework3.university;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Lecture {
+	
+	int lecnumber; //강의 번호
+	String name; //강의 이름
+	
+	
+	
+	
+}
